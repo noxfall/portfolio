334c39e1fad8014f3afe3d3305ba7715e935782e
Kain Noxfall <kain.noxfall@gmail.com>
2023-03-22 22:45:38 +0100

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
           <Navbar />
           <Hero />
         </div>
-        <About />
+        {/* <About /> */}
         {/*<Experience />*/}
         {/*<Tech />*/}
         {/*<Works />*/}
