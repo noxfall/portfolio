130cebb34325f54d32cb69b40338f61f3b0783f5
Kain Noxfall <kain.noxfall@gmail.com>
2023-03-22 22:28:50 +0100

    Recomposition

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './scenes/Hero';
-import About from './scenes/About';
+// import About from './scenes/About';
 
 const App = () => {
   return (
-  <BrowserRouter>
+    <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
-      <About />
-    {/*<Experience />*/}
-    {/*<Tech />*/}
-    {/*<Works />*/}
-    {/*<Feedbacks />*/}
-      <div className="relative z-0">
-    {/*<Contact />*/}
-    {/*<StarsCanvas />*/}
+        <About />
+        {/*<Experience />*/}
+        {/*<Tech />*/}
+        {/*<Works />*/}
+        {/*<Feedbacks />*/}
+        <div className="relative z-0">
+          {/*<Contact />*/}
+          {/*<StarsCanvas />*/}
+        </div>
       </div>
-    </div>
-  </BrowserRouter>
+    </BrowserRouter>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
           <img src='/logo.svg' alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Milos &nbsp;
-            <span className='sm:block hidden'> | Noxfall Labs, Inc.</span>
+            <span className='sm:block hidden'>| Noxfall Labs, Inc.</span>
           </p>
         </Link>
 
